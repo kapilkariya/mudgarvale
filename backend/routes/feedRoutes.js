242feef8f66c470128385a080bfed1aaea19f660
Kapil Kariya <147917943+kapilkariya@users.noreply.github.com>
2026-06-22 11:32:30 +0530

    feedroutes

--- backend/routes/feedRoutes.js
+++ backend/routes/feedRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const Product = require("../models/productModel");
+const Product = require("../models/Product"); // use the same model as controller
 
 const router = express.Router();
 
 router.get("/google-shopping-feed.xml", async (req, res) => {
   try {
-    const products = await Product.find({ isActive: true });
+    const products = await Product.find({ isActive: true }).lean();
 
     let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
@@ -15,20 +15,23 @@ router.get("/google-shopping-feed.xml", async (req, res) => {
 <description>Product Feed</description>`;
 
     products.forEach((product) => {
-      Object.entries(product.pricePerWeight).forEach(([weight, price]) => {
+      const prices = product.pricePerWeight || {};
+
+      Object.entries(prices).forEach(([weight, price]) => {
         xml += `
 <item>
 <g:id>${product._id}-${weight}kg</g:id>
-<title>${product.name}</title>
-<description><![CDATA[${product.description}]]></description>
-<link>https://mudgarvale.com/product/${product._id}</link>
+<g:title><![CDATA[${product.name}]]></g:title>
+<g:description><![CDATA[${product.description}]]></g:description>
+<g:link>https://mudgarvale.com/product/${product._id}</g:link>
 <g:image_link>${product.image}</g:image_link>
-<g:availability>in stock</g:availability>
+<g:availability>in_stock</g:availability>
+<g:condition>new</g:condition>
 <g:price>${price} INR</g:price>
 <g:brand>MudgarVale</g:brand>
-<g:condition>new</g:condition>
 <g:item_group_id>${product._id}</g:item_group_id>
 <g:size>${weight} KG</g:size>
+<g:product_type>${product.category}</g:product_type>
 </item>`;
       });
     });
@@ -39,9 +42,10 @@ router.get("/google-shopping-feed.xml", async (req, res) => {
 
     res.set("Content-Type", "application/xml");
     res.send(xml);
+
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Feed Error");
+    console.error("FEED ERROR:", err);
+    res.status(500).send(err.message);
   }
 });
 
